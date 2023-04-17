07a907952ae8f04cf53c34ca7cef13ccf5deb8fc
Matt Filetto <matt.filetto@gmail.com>
2023-04-16 18:34:32 -0700

    UdfpsSettings: Nuke refs to screen off udfps customizations.
    * we don't have this feature
    
    Change-Id: Ifc43d24b7b8c396ac765b0ffe3a68ede8c56de5d

--- src/com/rising/settings/fragments/lockscreen/UdfpsSettings.java
+++ src/com/rising/settings/fragments/lockscreen/UdfpsSettings.java
@@ -47,10 +47,10 @@ import com.android.settings.SettingsPreferenceFragment;
 public class UdfpsSettings extends SettingsPreferenceFragment {
 
     private static final String UDFPS_ANIM_PREVIEW = "udfps_recognizing_animation_preview";
-    private static final String SCREEN_OFF_UDFPS_ENABLED = "screen_off_udfps_enabled";
+
 
     private Preference mUdfpsAnimPreview;
-    private Preference mScreenOffUdfps;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -67,12 +67,6 @@ public class UdfpsSettings extends SettingsPreferenceFragment {
             prefSet.removePreference(mUdfpsAnimPreview);
         }
 
-        mScreenOffUdfps = (Preference) prefSet.findPreference(SCREEN_OFF_UDFPS_ENABLED);
-        boolean mScreenOffUdfpsAvailable = resources.getBoolean(
-                com.android.internal.R.bool.config_supportScreenOffUdfps);
-        if (!mScreenOffUdfpsAvailable)
-            prefSet.removePreference(mScreenOffUdfps);
-    }
 
     public static void reset(Context mContext) {
         ContentResolver resolver = mContext.getContentResolver();
@@ -82,8 +76,6 @@ public class UdfpsSettings extends SettingsPreferenceFragment {
                 Settings.System.UDFPS_ANIM_STYLE, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.UDFPS_ICON, 0, UserHandle.USER_CURRENT);
-        Settings.Secure.putIntForUser(resolver,
-                Settings.Secure.SCREEN_OFF_UDFPS_ENABLED, 0, UserHandle.USER_CURRENT);
     }
 
     @Override
